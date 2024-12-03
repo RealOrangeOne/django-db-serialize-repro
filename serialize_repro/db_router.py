a0a3a77c4b64df6227a7154ca1c02c8539e76952
Jake Howard <jake.howard@torchbox.com>
2024-12-03 10:43:56 +0000

    Don't run migrations against replica

--- serialize_repro/db_router.py
+++ serialize_repro/db_router.py
@@ -7,3 +7,7 @@ class ReadReplicaRouter:
             return "default"
 
         return "replica"
+
+    def allow_migrate(self, db, app_label, **hints):
+        # This method doesn't affect the bug, but a good idea anyway
+        return db != "replica"
